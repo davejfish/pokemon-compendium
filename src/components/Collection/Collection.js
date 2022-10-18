f14b80167a32c9e4d48587b8326b670668080db1
Hathaway-Shan <hathaway.shan@gmail.com>
2022-10-18 14:19:32 -0700

    updated loading animation

--- src/components/Collection/Collection.js
+++ src/components/Collection/Collection.js
@@ -10,13 +10,13 @@ export default function Collection() {
   const { gladiators, error, loading, types, setSelectedType, setName } = useGladiators();
   return (
     <div className="main">
+      <span>{loading ? <Pokeball /> : <></>}</span>
       <h2 className="title">gladiator collection</h2>
       <span>{error.length === 0 ? <>{error}</> : <></>}</span>
       <div className="controls">
         <Select options={types} changeHandler={setSelectedType} />
         <Search queryHandler={setName} />
       </div>
-      <span>{loading ? <Pokeball /> : <></>}</span>
       <div className="collection">
         {gladiators.map((gladiator) => (
           <Gladiator key={gladiator.id} {...gladiator} />
--- src/components/Pokeball/Pokeball.js
+++ src/components/Pokeball/Pokeball.js
@@ -5,8 +5,19 @@ export default function Pokeball() {
   return (
     <div className="body">
       <div className="container">
-        <div className="loadingText">Loading...</div>
         <div className="pokeball"></div>
+        <div className="loadingText">
+          <div className="loading_letter">L</div>
+          <div className="loading_letter">o</div>
+          <div className="loading_letter">a</div>
+          <div className="loading_letter">d</div>
+          <div className="loading_letter">i</div>
+          <div className="loading_letter">n</div>
+          <div className="loading_letter">g</div>
+          <div className="loading_letter">.</div>
+          <div className="loading_letter">.</div>
+          <div className="loading_letter">.</div>
+        </div>
       </div>
     </div>
   );
